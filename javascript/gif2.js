bd1b6f664995d76dee0aae40a66a3191478aaaf2
Jenny-Le <lejenny75@gmail.com>
2017-12-03 12:40:47 -0700

    firstpush

---
+++ javascript/gif2.js
@@ -0,0 +1,71 @@
+    $(document).ready(function() {
+        $("button").on("click", function() {
+            var topic = $(this).attr("data");
+
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+            var myDiv = $("<div>");
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log(response);
+
+                $("#gifs-appear-here").empty();
+
+                var results = response.data
+
+                for (var i = 0; i < results.length; i++) { //creating a for loop for my gifs
+
+                    var rating = results[i].rating;
+                    console.log(rating);
+
+                    var myImage = $("<img>"); // creating a var for my img tag
+                    myImage.attr("src", results[i].images.fixed_height.url);
+                    myDiv.append(myImage); // appending my div + image at the end!  
+                    $("#gifs-appear-here").prepend(myDiv); //prepending my gifs to start adding to the start of my div
+                }
+            });
+        });
+        var myButton = $('#add-gif');
+        myButton.on("click", function(e) {
+            e.preventDefault();
+            console.log($("#gif-input").val())
+            console.log("anything")
+
+            var topic = $("#gif-input").val();
+
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+            var myDiv = $("<div>");
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+                console.log(response);
+
+                $("#gifs-appear-here").empty();
+
+                var results = response.data
+
+                for (var i = 0; i < results.length; i++) { //creating a for loop for my gifs
+
+                    var rating = results[i].rating;
+                    console.log(rating);
+
+                    var myImage = $("<img>"); // creating a var for my img tag
+                    myImage.attr("src", results[i].images.fixed_height.url);
+                    myDiv.append(myImage); // appending my div + image at the end!  
+                    $("#gifs-appear-here").prepend(myDiv); //prepending my gifs to start adding to the start of my div
+                }
+            });
+
+
+        })
+
+
+    })
