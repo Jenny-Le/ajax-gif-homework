1a5cb937a2336cd707d1c69f02170f8e68ef6a90
Jenny-Le <lejenny75@gmail.com>
2017-12-05 18:35:58 -0700

    finalpush

--- javascript/gif2.js
+++ javascript/gif2.js
@@ -1,12 +1,13 @@
     $(document).ready(function() {
         $("button").on("click", function() {
+            // "this" is the on click event
             var topic = $(this).attr("data");
-
+            //quert and API url
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
                 topic + "&api_key=dc6zaTOxFJmzC&limit=10";
-
+            // creating a div tag
             var myDiv = $("<div>");
-
+            // ajax code
             $.ajax({
                 url: queryURL,
                 method: "GET"
@@ -14,7 +15,7 @@
                 console.log(response);
 
                 $("#gifs-appear-here").empty();
-
+                // response.data is the info you get from the API
                 var results = response.data
 
                 for (var i = 0; i < results.length; i++) { //creating a for loop for my gifs
